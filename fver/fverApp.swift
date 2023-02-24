27fe4f5ac7bad90fb1f6e2ed5de83aa7c7fcaa8a
raghad <raghad.k.alsaif@gmail.com>
2023-02-24 21:54:54 +0300

    Initial Commit

---
+++ fver/fverApp.swift
@@ -0,0 +1,17 @@
+//
+//  fverApp.swift
+//  fver
+//
+//  Created by raghad khalid alsaif on 04/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct fverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
